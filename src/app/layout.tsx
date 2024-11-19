a16174bb11ac227d4258348ab6f0213d60652940
saeidex <ebrahim.khalil.nayeem@gmail.com>
2024-11-19 07:30:00 +0600

    add: bg gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-gradient antialiased`}
       >
         <div className="flex h-dvh w-dvw flex-col">
           <NavigationMenu />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        gradient: "url('/gradient.jpg')",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
